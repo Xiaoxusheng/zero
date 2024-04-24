a30e30bdac94a4b90fa18a8fd28dae2d59c67d1e
lei <3096407768@qq.com>
2024-04-24 22:08:59 +0800

    增加视频模块

--- video/rpc/video.go
+++ video/rpc/video.go
@@ -26,7 +26,7 @@ func main() {
 	ctx := svc.NewServiceContext(c)
 
 	s := zrpc.MustNewServer(c.RpcServerConf, func(grpcServer *grpc.Server) {
-		video.RegisterVideoServer(grpcServer, server.NewVideoServer(ctx))
+		video.RegisterVideoServerServer(grpcServer, server.NewVideoServerServer(ctx))
 
 		if c.Mode == service.DevMode || c.Mode == service.TestMode {
 			reflection.Register(grpcServer)
